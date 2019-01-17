e081dd611c894b6810336afb70934f9d40f3b66c
phungphuc <phungthienphuc18@gmail.com>
2019-01-18 00:17:28 +0700

    first line

---
+++ index.js
@@ -0,0 +1,78 @@
+// Chapter 1: intro to programming
+
+// functional programming
+
+function human() {
+    talk();
+    dumpshit();
+    seeNresponse();
+}
+
+function talk() {
+    console.log();
+    // mo mieng ra 
+    // cuddfadfawef;asdkjfa
+    // ...
+}
+
+function dumpshit() {
+    // coi quan 
+    // ngoi len bon cau 
+    // ia 
+    // ...
+
+}
+
+function see() {
+
+}
+
+function seeNresponse() {
+    see();
+    talk();
+}
+
+function count(food1, food2) {
+    var result = food1 + food2;
+    return result;
+}
+
+
+// Object oriented programming
+
+// lap trinh huong doi tuong
+
+// Object human
+
+// Human > Dau > Minh > Chan
+// Class
+
+// Human :
+// porperty: sex, age, race
+// method: eat sleep run
+// var sex, age, race;
+// function eat() {
+
+// }
+
+// human.eat();
+// human.sex;
+
+// function sleep() { }
+
+// function run() { }
+
+// Dau:
+// toc mat mui mieng
+// class dauTron() {
+// size();
+// }
+
+// class dauVuong() {
+// size()
+// }
+// class dauOvan () {
+//     size();
+// }
+// Property :
+// Method: 
